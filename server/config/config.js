556e05a06449c03e6bb3f0cdd51e19cd37ce78a6
Jorge Avila <jorge.avila@alphacredit.mx>
2020-10-14 18:45:08 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === 'dev'){
      urlDB = 'mongodb://localhost:27017/cafe';
      } else {
         urlDB = process.env.MONGO_URI;
-        
      }
 
     process.env.URLDB = urlDB;
